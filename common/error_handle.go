168c7f5c1546eabfeca42b6a60cdb3e5a708ae97
Andras Elso <elso.andras@gmail.com>
2024-09-07 15:48:16 +0200

    Dont print error if message is empty

--- common/error_handle.go
+++ common/error_handle.go
@@ -7,14 +7,16 @@ import (
 )
 
 func CheckErr(err error, str string, v ...interface{}) {
-	if err != nil {
+	if err != nil && len(str) > 0 {
 		log.Fatalf("Error %s: %s", fmt.Sprintf(str, v...), err.Error())
 	}
 }
 
 func PrintErr(err error, str string, v ...interface{}) bool {
 	if err != nil {
-		log.Printf("Error %s: %s", fmt.Sprintf(str, v...), err.Error())
+		if len(str) > 0 {
+			log.Printf("Error %s: %s", fmt.Sprintf(str, v...), err.Error())
+		}
 		return true
 	}
 	return false
